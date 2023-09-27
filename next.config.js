c4d62a0085bcfe1cb74581393f5bb54861035282
Rodolfo Guluarte Hale <guluarte@gmail.com>
2023-09-27 04:45:31 -0600

    wip

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/jobflow',
+  basePath: '/',
   swcMinify: true,
   experimental: {
     serverActions: true,
