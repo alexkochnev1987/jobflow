6d2fb16c91d91af22c1d6012caa08d1daba89a18
Rodolfo Guluarte Hale <guluarte@gmail.com>
2023-09-29 14:59:25 -0600

    adds profile tab

--- app/profile/profile.tsx
+++ app/profile/profile.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Box, Button, Tabs, Text } from "@radix-ui/themes"
+import { Avatar, Badge, Box, Button, Card, Tabs, Text } from "@radix-ui/themes"
 import { useResults } from "@/lib/hooks/use-results"
 import { Flex, Heading } from "@radix-ui/themes"
 
@@ -35,6 +35,27 @@ export function ProfileResults({ results }) {
 
 export function ProfileTabs() {
   const { isLoading, results } = useResults()
+
+  const werte = [
+    "Hilfsbereitschaft",
+    "Kreativität",
+    "Mut",
+    "Pünktlichkeit",
+    "Präzision",
+    "Effizienz",
+  ]
+  const starken = [
+    "Geduld",
+    "Organisationsfähigkeiten",
+    "Kreativität",
+    "Planungsfähigkeiten",
+  ]
+
+  const idealeArbeitsrahmen = [
+    "Büroarbeit",
+    "Unabhängig arbeiten",
+    "Abwechslungsreiche Tätigkeit",
+  ]
   return (
     <>
       {isLoading && (
@@ -46,11 +67,10 @@ export function ProfileTabs() {
         </div>
       )}
       {!isLoading && (
-        <Tabs.Root defaultValue="Empfehlungen">
+        <Tabs.Root defaultValue="Empfehlungen" className="mx-auto w-2/3">
           <Tabs.List>
             <Tabs.Trigger value="Empfehlungen">Empfehlungen</Tabs.Trigger>
             <Tabs.Trigger value="Profil">Profil</Tabs.Trigger>
-            <Tabs.Trigger value="settings">Profil</Tabs.Trigger>
           </Tabs.List>
 
           <Box px="4" pt="3" pb="2">
@@ -59,13 +79,121 @@ export function ProfileTabs() {
             </Tabs.Content>
 
             <Tabs.Content value="Profil">
-              <Text size="2">Access and update your documents.</Text>
-            </Tabs.Content>
-
-            <Tabs.Content value="settings">
-              <Text size="2">
-                Edit your profile or update contact information.
-              </Text>
+              <Flex
+                direction="row"
+                className="mx-auto my-5 w-full"
+                align="stretch"
+              >
+                <Flex
+                  align="center"
+                  gap="2"
+                  direction="column"
+                  className="mx-10 w-1/5 text-center"
+                >
+                  <Avatar
+                    size="7"
+                    src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+                    fallback="S"
+                    radius="full"
+                  />
+                  <Heading size="5" className="">
+                    John Doe
+                  </Heading>
+                  <p>Hamburg, Germany</p>
+                  <Badge color="gray" radius="large" size="1" variant="solid">
+                    Hilfsbereitschaft
+                  </Badge>
+                </Flex>
+                <Flex className="mx-5 w-3/5" direction="column" gap="3">
+                  <Card variant="surface" className="w-full p-3">
+                    <Flex gap="3" align="start" direction="column">
+                      <Text as="div" size="2" weight="bold">
+                        Werte
+                      </Text>
+                      <Flex direction="row" gap="1">
+                        {werte.map((w) => (
+                          <Badge
+                            color="gray"
+                            radius="none"
+                            size="1"
+                            variant="solid"
+                            key={w}
+                          >
+                            {w}
+                          </Badge>
+                        ))}
+                      </Flex>
+                    </Flex>
+                  </Card>
+                  <Card variant="surface" className="w-full p-3">
+                    <Flex gap="3" align="start" direction="column">
+                      <Text as="div" size="2" weight="bold">
+                        Stärken
+                      </Text>
+                      <Flex direction="row" gap="1">
+                        {starken.map((w) => (
+                          <Badge
+                            color="gray"
+                            radius="none"
+                            size="1"
+                            variant="solid"
+                            key={w}
+                          >
+                            {w}
+                          </Badge>
+                        ))}
+                      </Flex>
+                    </Flex>
+                  </Card>
+                  <Card variant="surface" className="w-full p-3">
+                    <Flex gap="3" align="start" direction="column">
+                      <Text as="div" size="2" weight="bold">
+                        Ideale Arbeitsrahmen
+                      </Text>
+                      <Flex direction="row" gap="1">
+                        {idealeArbeitsrahmen.map((w) => (
+                          <Badge
+                            color="gray"
+                            radius="none"
+                            size="1"
+                            variant="solid"
+                            key={w}
+                          >
+                            {w}
+                          </Badge>
+                        ))}
+                      </Flex>
+                    </Flex>
+                  </Card>
+                  <Card variant="surface" className="w-full p-3">
+                    <Flex gap="3" align="start" direction="column">
+                      <Text as="div" size="2" weight="bold">
+                        Persönlichkeitstyp
+                      </Text>
+                      <Flex direction="row" gap="1">
+                        <Badge
+                          color="gray"
+                          radius="none"
+                          size="1"
+                          variant="solid"
+                        >
+                          INFP
+                        </Badge>
+                      </Flex>
+                    </Flex>
+                  </Card>
+                </Flex>
+                <Flex className="w-1/5">
+                  <Button
+                    size="3"
+                    variant="solid"
+                    radius="full"
+                    className="w-full"
+                  >
+                    Edit profile
+                  </Button>
+                </Flex>
+              </Flex>
             </Tabs.Content>
           </Box>
         </Tabs.Root>
