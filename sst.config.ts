09e5bce32a547dabb2f8a70e00aa6eed778dc93c
Rodolfo Guluarte Hale <guluarte@gmail.com>
2023-10-18 00:05:15 -0600

    increase timeout and set runtime

--- sst.config.ts
+++ sst.config.ts
@@ -1,20 +1,23 @@
-import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { SSTConfig } from "sst"
+import { NextjsSite } from "sst/constructs"
 
 export default {
   config(_input) {
     return {
       name: "jobflow",
       region: "eu-central-1",
-    };
+    }
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+        timeout: 120,
+        runtime: "nodejs18.x",
+      })
 
       stack.addOutputs({
         SiteUrl: site.url,
-      });
-    });
+      })
+    })
   },
-} satisfies SSTConfig;
+} satisfies SSTConfig
