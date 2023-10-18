ba843765680fafb44f847e11fbebafa783bd46e9
Rodolfo Guluarte Hale <guluarte@gmail.com>
2023-10-18 00:58:29 -0600

    adds strapi

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,5 @@
 import { SSTConfig } from "sst"
-import { NextjsSite } from "sst/constructs"
+import { NextjsSite, Service } from "sst/constructs"
 
 export default {
   config(_input) {
@@ -10,6 +10,20 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
+      const strapi = new Service(stack, "strapi", {
+        path: "./strapi",
+        port: 1337,
+        cpu: "0.25 vCPU",
+        memory: "0.5 GB",
+        // environment: {
+        //   DATABASE_CLIENT: process.env.DATABASE_CLIENT,
+        //   DATABASE_HOST: process.env.DATABASE_HOST,
+        //   DATABASE_PORT: process.env.DATABASE_PORT,
+        //   DATABASE_NAME: process.env.DATABASE_NAME,
+        //   DATABASE_USERNAME: process.env.DATABASE_USERNAME,
+        //   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+        // },
+      })
       const site = new NextjsSite(stack, "site", {
         timeout: 120,
         runtime: "nodejs18.x",
@@ -17,6 +31,7 @@ export default {
 
       stack.addOutputs({
         SiteUrl: site.url,
+        Service: strapi.id,
       })
     })
   },
