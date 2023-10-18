68c86983819a50102f7505d41167383ee4b26f4e
Rodolfo Guluarte Hale <guluarte@gmail.com>
2023-10-18 01:08:05 -0600

    strapi.url

--- sst.config.ts
+++ sst.config.ts
@@ -15,23 +15,18 @@ export default {
         port: 1337,
         cpu: "0.25 vCPU",
         memory: "0.5 GB",
-        // environment: {
-        //   DATABASE_CLIENT: process.env.DATABASE_CLIENT,
-        //   DATABASE_HOST: process.env.DATABASE_HOST,
-        //   DATABASE_PORT: process.env.DATABASE_PORT,
-        //   DATABASE_NAME: process.env.DATABASE_NAME,
-        //   DATABASE_USERNAME: process.env.DATABASE_USERNAME,
-        //   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
-        // },
       })
       const site = new NextjsSite(stack, "site", {
         timeout: 120,
         runtime: "nodejs18.x",
+        environment: {
+          STRAPI_URL: strapi.url,
+        },
       })
 
       stack.addOutputs({
         SiteUrl: site.url,
-        Service: strapi.id,
+        Service: strapi.url,
       })
     })
   },
