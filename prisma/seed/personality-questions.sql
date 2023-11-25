13bd74357dd0d8ded5f6700fcf46904d30fe233d
Rodolfo Guluarte Hale <guluarte@gmail.com>
2023-11-24 19:27:58 -0600

    Adds SQL for personality questions

---
+++ prisma/seed/personality-questions.sql
@@ -0,0 +1,139 @@
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 0, 1, 'At a party do you:', 'Interact with many, including strangers', 'Interact with a few, known to you');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 1, 2, 'Are you more:', 'Realistic than speculative', 'Speculative than realistic');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 2, 3, 'Is it worse to:', 'Have your “head in the clouds”', 'Be “in a rut”');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 3, 4, 'Are you more impressed by:', 'Principles', 'Emotions');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 4, 5, 'Are more drawn toward the:', 'Convincing', 'Touching');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 5, 6, 'Do you prefer to work:', 'To deadlines', 'Just “whenever”');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 6, 7, 'Do you tend to choose:', 'Rather carefully', 'Somewhat impulsively');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 7, 1, 'At parties do you:', 'Stay late, with increasing energy', 'Leave early with decreased energy');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 8, 2, 'Are you more attracted to:', 'Sensible people', 'Imaginative people');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 9, 3, 'Are you more interested in:', 'What is actual', 'What is possible');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 10, 4, 'In judging others are you more swayed by:', 'Laws than circumstances', 'Circumstances than laws');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 11, 5, 'In approaching others is your inclination to be somewhat:', 'Objective', 'Personal');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 12, 6, 'Are you more:', 'Punctual', 'Leisurely');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 13, 7, 'Does it bother you more having things:', 'Incomplete', 'Completed');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 14, 1, 'In your social groups do you:', 'Keep abreast of other’s happenings', 'Get behind on the news');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 15, 2, 'In doing ordinary things are you more likely to:', 'Do it the usual way', 'Do it your own way');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 16, 3, 'Writers should:', '“Say what they mean and mean what they say”', 'Express things more by use of analogy');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 17, 4, 'Which appeals to you more:', 'Consistency of thought', 'Harmonious human relationships');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 18, 5, 'Are you more comfortable in making:', 'Logical judgments', 'Value judgments');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 19, 6, 'Do you want things:', 'Settled and decided', 'Unsettled and undecided');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 20, 7, 'Would you say you are more:', 'Serious and determined', 'Easy-going');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 21, 1, 'In phoning do you:', 'Rarely question that it will all be said', 'Rehearse what you’ll say');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 22, 2, 'Facts:', '“Speak for themselves”', 'Illustrate principles');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 23, 3, 'Are visionaries:', 'somewhat annoying', 'rather fascinating');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 24, 4, 'Are you more often:', 'a cool-headed person', 'a warm-hearted person');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 25, 5, 'Is it worse to be:', 'unjust', 'merciless');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 26, 6, 'Should one usually let events occur:', 'by careful selection and choice', 'randomly and by chance');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 27, 7, 'Do you feel better about:', 'having purchased', 'having the option to buy');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 28, 1, 'In company do you:', 'initiate conversation', 'wait to be approached');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 29, 2, 'Common sense is:', 'rarely questionable', 'frequently questionable');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 30, 3, 'Children often do not:', 'make themselves useful enough', 'exercise their fantasy enough');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 31, 4, 'In making decisions do you feel more comfortable with:', 'standards', 'feelings');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 32, 5, 'Are you more:', 'firm than gentle', 'gentle than firm');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 33, 6, 'Which is more admirable:', 'the ability to organize and be methodical', 'the ability to adapt and make do');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 34, 7, 'Do you put more value on:', 'infinite', 'open-minded');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 35, 1, 'Does new and non-routine interaction with others:', 'stimulate and energize you', 'tax your reserves');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 36, 2, 'Are you more frequently:', 'a practical sort of person', 'a fanciful sort of person');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 37, 3, 'Are you more likely to:', 'see how others are useful', 'see how others see');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 38, 4, 'Which is more satisfying:', 'to discuss an issue thoroughly', 'to arrive at agreement on an issue');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 39, 5, 'Which rules you more:', 'your head', 'your heart');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 40, 6, 'Are you more comfortable with work that is:', 'contracted', 'done on a casual basis');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 41, 7, 'Do you tend to look for:', 'the orderly', 'whatever turns up');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 42, 1, 'Do you prefer:', 'many friends with brief contact', 'a few friends with more lengthy contact');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 43, 2, 'Do you go more by:', 'facts', 'principles');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 44, 3, 'Are you more interested in:', 'production and distribution', 'design and research');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 45, 4, 'Which is more of a compliment:', '“There is a very logical person.”', '“There is a very sentimental person.”');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 46, 5, 'Do you value in yourself more that you are:', 'unwavering', 'devoted');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 47, 6, 'Do you more often prefer the:', 'final and unalterable statement', 'tentative and preliminary statement');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 48, 7, 'Are you more comfortable:', 'after a decision', 'before a decision');  
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 49, 1, 'Do you:', 'speak easily and at length with strangers', 'find little to say to strangers');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 50, 2, 'Are you more likely to trust your:', 'experience', 'hunch');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 51, 3, 'Do you feel:', 'more practical than ingenious', 'more ingenious than practical');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 52, 4, 'Which person is more to be complimented – one of:', 'clear reason', 'strong feeling');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 53, 5, 'Are you inclined more to be:', 'fair-minded', 'sympathetic');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 54, 6, 'Is it preferable mostly to:', 'make sure things are arranged', 'just let things happen');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 55, 7, 'In relationships should most things be:', 're-negotiable', 'random and circumstantial');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 56, 1, 'When the phone rings do you:', 'hasten to get to it first', 'hope someone else will answer');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 57, 2, 'Do you prize more in yourself:', 'a strong sense of reality', 'a vivid imagination');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 58, 3, 'Are you drawn more to:', 'fundamentals', 'overtones');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 59, 4, 'Which seems the greater error:', 'to be too passionate', 'to be too objective');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 60, 5, 'Do you see yourself as basically:', 'hard-headed', 'soft-hearted'); 
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 61, 6, 'Which situation appeals to you more:','the structured and scheduled', 'the unstructured and unscheduled');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 62, 7, 'Are you a person that is more:', 'routinized than whimsical','whimsical than routinized');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 63, 1, 'Are you more inclined to be:', 'easy to approach','somewhat reserved');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 64, 2, 'In writings do you prefer:', 'the more literal','the more figurative');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 65, 3, 'Is it harder for you to:','identify with others','utilize others');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 66, 4, 'Which do you wish more for yourself:', 'clarity of reason','strength of compassion');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 67, 5, 'Which is the greater fault:','being indiscriminate','being critical'); 
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 68, 6, 'Do you prefer the:','planned event','unplanned event');
+
+INSERT INTO jobflow.UserPersonalityQuestion (status, sort, `column`, question, a, b) VALUES('published', 69, 7, 'Do you tend to be more:','deliberate than spontaneous','spontaneous than deliberate');
