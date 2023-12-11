f872746ccbd6e9358e086055d56f97e54a32660d
Rodolfo Guluarte Hale <guluarte@gmail.com>
2023-12-11 11:03:50 -0600

    Update MatchingCareerCard stories layout and add description

--- components/shared/matching-career.stories.tsx
+++ components/shared/matching-career.stories.tsx
@@ -2,15 +2,21 @@ import type { Meta, StoryObj } from "@storybook/react"
 
 import MatchingCareerCard from "./matching-career"
 
-
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "MatchingCareerCard",
   component: MatchingCareerCard,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-    layout: "centered",
+    layout: "fullscreen",
   },
+  decorators: [
+    (Story) => (
+      <div className="flex flex-col items-center justify-center min-h-screen max-w-5xl m-auto">
+        <Story />
+      </div>
+    ),
+  ],
   argTypes: {
     hidden: {
       control: {
@@ -38,6 +44,7 @@ const meta = {
         salaryMin: 2000,
         salaryMax: 3000,
         retrainingWeeks: 12,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       },
     },
   },
@@ -48,5 +55,14 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const MatchingCareerCardShow: Story = {
+  args: {
+    hidden: false,
+  },
+}
+
 export const MatchingCareerCardHidden: Story = {
+  args: {
+    hidden: true,
+  },
 }
