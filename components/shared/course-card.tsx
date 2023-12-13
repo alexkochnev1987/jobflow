d60c0d5b10cd8c3d318494edaaac8faf875f9448
Rodolfo Guluarte Hale <guluarte@gmail.com>
2023-12-13 11:04:09 -0600

    Refactor CourseCard component layout and styling

--- components/shared/course-card.tsx
+++ components/shared/course-card.tsx
@@ -43,8 +43,7 @@ export default function CourseCard({
   }
   return (
     <>
-      {/* mobile */}
-      <div className="flex w-full flex-col items-end rounded-2xl border border-solid border-[color:var(--grey-300,#D0D5DD)] bg-white p-6 shadow-sm max-md:px-5 md:hidden">
+      <div className="w- flex flex-col items-end rounded-2xl border border-solid border-[color:var(--grey-300,#D0D5DD)] bg-white p-6 shadow-sm max-md:px-5 md:w-1/2 lg:w-1/3 ">
         <Image
           height={120}
           width={120}
@@ -101,79 +100,9 @@ export default function CourseCard({
           {description}
         </div>
         <div className="flex flex-row items-stretch justify-between gap-1 self-stretch">
-          <Button intent="outlined">Contact</Button>
-          <Button intent="primary">Enroll</Button>
-        </div>
-      </div>
-
-      {/* desktop */}
-      <div className="hidden w-full flex-col items-end rounded-2xl border border-solid border-[color:var(--grey-300,#D0D5DD)] bg-white p-6 shadow-sm max-md:px-5 md:visible md:flex">
-        <div className="flex flex-row">
-          <div className="w-1/5 justify-center align-middle">
-            <Image
-              height={120}
-              width={120}
-              className="m-auto shrink grow basis-0 "
-              src="https://placehold.co/120x120"
-              placeholder="empty"
-              alt=""
-            />
-          </div>
-          <div className="w-3/5">
-            <div className="mt-3 self-stretch whitespace-nowrap text-left text-xs font-medium leading-5 text-gray-500 max-md:max-w-full">
-              COURSE
-            </div>
-            <div className="mt-1 self-stretch text-xl font-bold leading-8 text-black max-md:max-w-full">
-              {name}
-            </div>
-            <div className="mt-4 self-stretch text-sm leading-6 text-slate-700 max-md:max-w-full">
-              {description}
-            </div>
-          </div>
-          <div>
-            <div className="mt-1 flex items-center gap-2 self-start">
-              <Calendar />
-              <div className="text-sm font-medium leading-5 text-slate-700">
-                <span className="font-semibold">Start date:</span>
-                <span className="capitalize"> {startDate}</span>
-              </div>
-            </div>
-            <div className="mt-1.5 flex items-center gap-2 self-start">
-              <Clock className="fill-slate-700" />
-              <div className="grow self-stretch whitespace-nowrap text-sm leading-6 text-slate-700">
-                <span className="font-semibold">Pace:</span>
-                <span className=""> {pace}</span>
-              </div>
-            </div>
-            <div className="mt-1.5 flex items-center gap-2 self-start pr-14 max-md:pr-5">
-              <Location />
-              <div className="grow self-stretch whitespace-nowrap text-sm leading-6 text-slate-700">
-                <span className="font-semibold">Location:</span>
-                <span className=""> {location}</span>
-              </div>
-            </div>
-            <div className="mt-1.5 flex items-center gap-2 self-start">
-              <Tag />
-              <div className="grow self-stretch whitespace-nowrap text-sm leading-6 text-slate-700">
-                <span className="font-semibold">Price: </span>
-                <span className=""> {salaryFormatter(price)} €</span>
-              </div>
-            </div>
-            <div className="ml-5 mt-1 self-start whitespace-nowrap text-sm leading-6 text-sky-500 max-md:ml-2.5">
-              Want it for free?
-            </div>
-            <div className="mt-1.5 flex items-center gap-2 self-start pr-7 max-md:pr-5">
-              <Bag />
-              <div className="grow self-stretch whitespace-nowrap text-sm leading-6 text-slate-700">
-                <span className="font-semibold">Sponsorship:</span>
-                <span className=""> {sponsorship ? "Yes" : "No"}</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="mt-8 flex w-2/5 justify-end gap-1">
-          <Button intent="outlined">Contact</Button>
-          <Button intent="primary">Enroll</Button>
+          <Button intent="primary" className="bg-rose-500 hover:bg-rose-400">
+            Enroll
+          </Button>
         </div>
       </div>
     </>
