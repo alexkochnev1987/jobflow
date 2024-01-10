615216383ae2be91b12d25c7b54339e1eda69d91
Rodolfo Guluarte Hale <guluarte@gmail.com>
2024-01-09 22:12:14 -0600

    mobile menu

--- components/layout/user-dropdown.tsx
+++ components/layout/user-dropdown.tsx
@@ -2,13 +2,22 @@
 
 import { useState } from "react"
 import { signOut } from "next-auth/react"
-import {  LayoutDashboard, LogOut, Settings } from "lucide-react"
+import {
+  HelpCircle,
+  LayoutDashboard,
+  LogOut,
+  Menu,
+  Settings,
+  X,
+  XSquare,
+} from "lucide-react"
 import Popover from "@/components/shared/popover"
 import { Session } from "next-auth"
 import Link from "next/link"
 import { ROUTES } from "@/lib/constants"
 import l18n from "@/i18n/config"
 import UserIcon from "@/icons/user.svg"
+import { Flex, Separator } from "@radix-ui/themes"
 
 export default function UserDropdown({ session }: { session: Session }) {
   const { email, image } = session?.user || {}
@@ -21,6 +30,16 @@ export default function UserDropdown({ session }: { session: Session }) {
       <Popover
         content={
           <div className="w-full rounded-md bg-white sm:w-56">
+            <Flex direction="row" align="center" gap="2" justify="start" className="md:hidden">
+              <p className="ml-4 text-sm font-semibold leading-tight text-slate-700">
+                Account Menu
+              </p>
+              <X
+                onClick={() => setOpenPopover(false)}
+                className="stroke-gray-300"
+              />
+            </Flex>
+            <Separator my="3" size="4" className="md:hidden"/>
             <Link
               className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
               href={ROUTES.DashBoard}
@@ -34,6 +53,13 @@ export default function UserDropdown({ session }: { session: Session }) {
             >
               <Settings className="h-4 w-4" />
               <p className="text-sm">{l18n.t("Settings")}</p>
+            </Link>
+            <Link
+              className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
+              href={ROUTES.Contact}
+            >
+              <HelpCircle className="h-4 w-4" />
+              <p className="text-sm">{l18n.t("Support")}</p>
             </Link>
             <button
               className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
@@ -52,7 +78,8 @@ export default function UserDropdown({ session }: { session: Session }) {
           onClick={() => setOpenPopover(!openPopover)}
           className="flex h-8 w-8 items-center justify-center overflow-hidden rounded-full transition-all duration-75 focus:outline-none active:scale-95 sm:h-9 sm:w-9"
         >
-          <UserIcon />
+          <Menu className="stroke-white md:hidden" />
+          <UserIcon className="hidden md:block" />
         </button>
       </Popover>
     </div>
--- components/shared/leaflet.tsx
+++ components/shared/leaflet.tsx
@@ -13,7 +13,7 @@ export default function Leaflet({
   const transitionProps = { type: "spring", stiffness: 500, damping: 30 }
   useEffect(() => {
     controls.start({
-      y: 20,
+      x: -80,
       transition: transitionProps,
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -22,12 +22,12 @@ export default function Leaflet({
   async function handleDragEnd(_: any, info: any) {
     const offset = info.offset.y
     const velocity = info.velocity.y
-    const height = leafletRef.current?.getBoundingClientRect().height || 0
-    if (offset > height / 2 || velocity > 800) {
-      await controls.start({ y: "100%", transition: transitionProps })
+    const width = leafletRef.current?.getBoundingClientRect().width || 0
+    if (offset > width / 2 || velocity > 800) {
+      await controls.start({ x: "100%", transition: transitionProps })
       setShow(false)
     } else {
-      controls.start({ y: 0, transition: transitionProps })
+      controls.start({ x: 0, transition: transitionProps })
     }
   }
 
@@ -36,12 +36,12 @@ export default function Leaflet({
       <motion.div
         ref={leafletRef}
         key="leaflet"
-        className="group fixed inset-x-0 bottom-0 z-40 w-screen cursor-grab bg-white pb-5 active:cursor-grabbing sm:hidden"
-        initial={{ y: "100%" }}
+        className="group fixed inset-y-0 bottom-0 z-40 w-screen cursor-grab bg-white pb-5 active:cursor-grabbing sm:hidden"
+        initial={{ x: "100%" }}
         animate={controls}
-        exit={{ y: "100%" }}
+        exit={{ x: "100%" }}
         transition={transitionProps}
-        drag="y"
+        drag="x"
         dragDirectionLock
         onDragEnd={handleDragEnd}
         dragElastic={{ top: 0, bottom: 1 }}
