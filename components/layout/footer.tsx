f17713b52960b341b2cd5d6e9a1a0edbe9638249
Rodolfo Guluarte Hale <guluarte@gmail.com>
2024-01-11 12:56:38 -0600

    mobile footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,123 +1,54 @@
 import React from "react"
 import Logo from "@/icons/logo.svg"
 import l18n from "@/i18n/config"
+import { Flex, Grid } from "@radix-ui/themes"
 function FooterComponent() {
   return (
-    <footer className="mx-auto  flex  max-w-6xl flex-col items-stretch border-t border-solid border-t-[color:var(--grey-300,#D0D5DD)] bg-white px-20 py-12 max-md:px-5">
-      <div className="mt-3.5 max-md:max-w-full max-md:pr-5">
-        <div className="flex gap-5 max-md:flex-col max-md:items-stretch max-md:gap-0">
-          <div className="flex w-[18%] flex-col items-stretch max-md:ml-0 max-md:w-full">
-            <div className="flex grow flex-col items-stretch pb-12 max-md:mt-8">
-              <div className="text-base font-semibold leading-5 text-black">
-                {" "}
-                {l18n.t("footer.Media")}{" "}
-              </div>
-              <div className="mt-4 whitespace-nowrap text-base leading-6 text-gray-500">
-                {" "}
-                {l18n.t("footer.Stories")}{" "}
-              </div>
-              <div className="mt-3 whitespace-nowrap text-base leading-6 text-gray-500">
-                {" "}
-                {l18n.t("footer.ffiliate")}{" "}
-              </div>
-              <div className="mb-9 mt-3 whitespace-nowrap text-base leading-6 text-gray-500">
-                {" "}
-                {l18n.t("footer.Press")}{" "}
-              </div>
-            </div>
-          </div>
-          <div className="ml-5 flex w-[82%] flex-col items-stretch max-md:ml-0 max-md:w-full">
-            <div className="max-md:mt-8 max-md:max-w-full">
-              <div className="flex gap-5 max-md:flex-col max-md:items-stretch max-md:gap-0">
-                <div className="flex w-3/12 flex-col items-stretch max-md:ml-0 max-md:w-full">
-                  <div className="flex flex-col items-stretch max-md:mt-3.5">
-                    <div className="text-base font-semibold leading-5 text-black">
-                      {" "}
-                      {l18n.t("footer.Support")}{" "}
-                    </div>
-                    <div className="mt-4 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.Institutes")}{" "}
-                    </div>
-                    <div className="mt-3 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.Blog")}{" "}
-                    </div>
-                    <div className="mt-3 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.FAQ")}{" "}
-                    </div>
-                  </div>
-                </div>
-                <div className="ml-5 flex w-3/12 flex-col items-stretch max-md:ml-0 max-md:w-full">
-                  <div className="flex grow flex-col items-stretch pb-9 max-md:mt-3.5">
-                    <div className="text-base font-semibold leading-5 text-black">
-                      {" "}
-                      {l18n.t("footer.About")}{" "}
-                    </div>
-                    <div className="mt-4 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.Our Team")}{" "}
-                    </div>
-                    <div className="mt-3 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.Companies")}{" "}
-                    </div>
-                    <div className="mt-3 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.Philosophy")}{" "}
-                    </div>
-                  </div>
-                </div>
-                <div className="ml-5 flex w-3/12 flex-col items-stretch max-md:ml-0 max-md:w-full">
-                  <div className="flex flex-col items-stretch max-md:mt-3.5">
-                    <div className="text-base font-semibold leading-5 text-black">
-                      {" "}
-                      {l18n.t("footer.Social")}{" "}
-                    </div>
-                    <div className="mt-4 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.Twitter")}{" "}
-                    </div>
-                    <div className="mt-3 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.LinkedIn")}{" "}
-                    </div>
-                    <div className="mt-3 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.Facebook")}{" "}
-                    </div>
-                  </div>
-                </div>
-                <div className="ml-5 flex w-3/12 flex-col items-stretch max-md:ml-0 max-md:w-full">
-                  <div className="flex grow flex-col items-stretch max-md:mt-3.5">
-                    <div className="text-base font-semibold leading-5 text-black">
-                      {" "}
-                      {l18n.t("footer.Legal")}{" "}
-                    </div>
-                    <div className="mt-4 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.Terms")}{" "}
-                    </div>
-                    <div className="mt-3 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.Privacy")}{" "}
-                    </div>
-                    <div className="mt-3 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.Data Protection")}{" "}
-                    </div>
-                    <div className="mt-3 whitespace-nowrap text-base leading-6 text-gray-500">
-                      {" "}
-                      {l18n.t("footer.Contact")}{" "}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <footer className="mx-auto  flex  max-w-6xl flex-col items-stretch border-t border-solid border-t-[color:var(--grey-300,#D0D5DD)] bg-white px-20 py-12 text-gray-500 max-md:px-5">
+      <Flex className="w-full" direction="row" wrap="wrap">
+        <ul className="w-1/2 md:w-1/4 lg:w-1/5">
+          <li className="pb-1 text-base font-semibold leading-normal text-black">
+            {l18n.t("footer.Media", "Media")}
+          </li>
+          <li>{l18n.t("footer.affiliate")}</li>
+          <li>{l18n.t("footer.stories")}</li>
+          <li>{l18n.t("footer.press")}</li>
+        </ul>
+        <ul className="w-1/2 md:w-1/4 lg:w-1/5">
+          <li className="pb-1 text-base font-semibold leading-normal text-black">
+            {l18n.t("footer.support")}
+          </li>
+          <li>{l18n.t("footer.institutes")}</li>
+          <li>{l18n.t("footer.blog")}</li>
+          <li>{l18n.t("footer.FAQ")}</li>
+        </ul>
+        <ul className="mt-2 w-1/2 md:w-1/4 lg:w-1/5">
+          <li className="pb-1 text-base font-semibold leading-normal text-black">
+            {l18n.t("footer.About")}
+          </li>
+          <li>{l18n.t("footer.out-team")}</li>
+          <li>{l18n.t("footer.companies")}</li>
+          <li>{l18n.t("footer.philosophy")}</li>
+        </ul>
+        <ul className="mt-2 w-1/2 md:w-1/4 lg:w-1/5">
+          <li className="pb-1 text-base font-semibold leading-normal text-black">
+            {l18n.t("footer.social")}
+          </li>
+          <li>{l18n.t("footer.twitter")}</li>
+          <li>{l18n.t("footer.linkedin")}</li>
+          <li>{l18n.t("footer.facebook")}</li>
+        </ul>
+        <ul className="mt-2 w-1/2 md:w-1/4 lg:w-1/5">
+          <li className="pb-1 text-base font-semibold leading-normal text-black">
+            {l18n.t("footer.legal")}
+          </li>
+          <li>{l18n.t("footer.terms")}</li>
+          <li>{l18n.t("footer.privacy")}</li>
+          <li>{l18n.t("footer.data-protection")}</li>
+          <li>{l18n.t("footer.contact")}</li>
+        </ul>
+      </Flex>
+
       <div className="mt-10 flex flex-col  max-md:max-w-full max-md:px-5">
         <div className="mt-6 flex  justify-start gap-5 max-md:max-w-full max-md:flex-wrap">
           <Logo />
