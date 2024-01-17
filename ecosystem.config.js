f4271a632dc905f6be3a07c159c4f4b634966e2d
Rodolfo Guluarte Hale <guluarte@gmail.com>
2024-01-17 12:39:31 -0600

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,36 @@
+const execSync = require('child_process').execSync
+
+function fetchSecrets() {
+  try {
+    return JSON.parse(
+      execSync("doppler secrets download --no-file --format json"),
+    )
+  } catch (error) {
+    process.exit(1)
+  }
+}
+
+fetchSecrets()
+
 module.exports = {
-    apps: [
-        {
-            name: 'jobflow',
-            exec_mode: 'cluster',
-            instances: 1,
-            max_memory_restart: '500M',
-            script: 'node_modules/next/dist/bin/next',
-            args: 'start',
-            watch: true,
-            env_local: {
-                APP_ENV: 'local' // APP_ENV=local
-            },
-            env_development: {
-                APP_ENV: 'dev' // APP_ENV=dev
-            },
-            env_production: {
-                APP_ENV: 'prod' // APP_ENV=prod
-            }
-        }
-    ]
+  apps: [
+    {
+      name: "jobflow",
+      exec_mode: "cluster",
+      instances: 2,
+      max_memory_restart: "500M",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      watch: false,
+      env_local: {
+        APP_ENV: "local", // APP_ENV=local
+      },
+      env_development: {
+        APP_ENV: "dev", // APP_ENV=dev
+      },
+      env_production: {
+        APP_ENV: "prod", // APP_ENV=prod
+      },
+    },
+  ],
 }
