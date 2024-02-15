d2bf9e424fd699d476803a99a275c86529f1e927
Marcel Reperich <reperich@yay-digital.de>
2024-02-15 14:30:27 +0100

    fix: increase base font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,7 @@ module.exports = {
       },
       fontSize: {
         sm: "0.8rem",
-        base: "1rem",
+        base: "1.3rem",
         xl: "1.5rem",
         "2xl": "1.563rem",
         "3xl": "1.953rem",
