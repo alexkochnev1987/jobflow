f60a67851fe37e796087db137ab364816649e990
Marcel Reperich <reperich@yay-digital.de>
2024-02-27 12:57:15 +0100

    fix(register-page): adds background to register page

--- app/company/register/page.tsx
+++ app/company/register/page.tsx
@@ -2,6 +2,8 @@ import { RegisterForm } from "./form"
 import { ROUTES } from "@/lib/constants"
 import { redirect } from "next/navigation"
 import { auth } from "auth"
+import { Flex } from "@radix-ui/themes"
+import RegisterBackground from "./bg"
 
 export default async function RegisterPage() {
   const session = await auth()
@@ -9,5 +11,13 @@ export default async function RegisterPage() {
   if (session) {
     return redirect(ROUTES.DashBoard)
   }
-  return <RegisterForm />
+
+  return (
+    <div className="flex h-screen justify-between flex-col-reverse md:flex-row">
+    <RegisterBackground />
+    <Flex justify="center" align="center" className="mx-auto max-w-md w-full h-full">
+      <RegisterForm />
+    </Flex>
+    </div>
+  )
 }
